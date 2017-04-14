abb03f2a1ecf4c7836a7ea9d30a06a4af6036cfd
Cuong Ho <cuong.ict.10101@gmail.com>
2017-04-14 17:04:05 +0700

    Refactor theme code

--- mock/front/dist/js/mpf.dropdownanimation.js
+++
@@ -1,6 +0,0 @@
-﻿$(function () {
-    $('.dropdown')
-        .hover(function () { $(this).find('ul.dropdown-menu').slideDown('fast'); },
-               function () { $(this).find('ul.dropdown-menu').slideUp('fast'); });
-
-});
