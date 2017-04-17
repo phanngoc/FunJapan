628269e998aaa1966eabd35fab49610786f9b3a8
Ng The Thao <nguyen.the.thao@framgia.com>
2017-04-17 10:34:26 +0700

    Update design

--- resources/views/layouts/includes/scripts_toppage.blade.php
+++ resources/views/layouts/includes/scripts_toppage.blade.php
@@ -1,54 +1,27 @@
-{!! Html::script('assets/js/jquery-2.1.0.min.js') !!}
-{!! Html::script('assets/js/jquery-ui-1.11.4.js') !!}
-{!! Html::script('assets/js/bootstrap-3.1.1.js') !!}
-{!! Html::script('assets/js/bootstrap-fileinput/fileinput.js') !!}
-{!! Html::script('assets/js/bootstrap-lightbox/bootstrap-lightbox.js') !!}
-{!! Html::script('assets/js/jquery.cookie-1.4.1.js') !!}
-{!! Html::script('assets/js/mpf.core.js') !!}
-{!! Html::script('assets/js/mpf.linkbuttonbase.min.js') !!}
-{!! Html::script('assets/js/mpf.textanimation.min.js') !!}
-{!! Html::script('assets/js/mpf.analytics.js') !!}
-{!! Html::script('assets/js/mpf.googleanalytics.event.js') !!}
-{!! Html::script('assets/js/mpf.userpreference.js') !!}
-{!! Html::script('assets/js/Page/Account/BasicProfile.js') !!}
-{!! Html::script('assets/js/jquery.mpf.favoritebutton.js') !!}
-{!! Html::script('assets/js/mpf.sharebutton.js') !!}
-{!! Html::script('assets/js/mpf.commentcount.js') !!}
-{!! Html::script('assets/js/mpf.articlephoto.js') !!}
-<!-- Slider -->
-{!! Html::script('assets/js/mpf.slider.js') !!}
-<!-- Slick carousel -->
-{!! Html::script('assets/js/mpf.slick-carousel.js') !!}
-<!-- Infinite scroll -->
-{!! Html::script('assets/js/infinite-scroll/jquery.infinitescroll.js') !!}
-<!-- Components-->
-<!--Header-->
-{!! Html::script('assets/js/mpf.navbar.js') !!}
-{!! Html::script('assets/js/mpf.slidecategory.js') !!}
-<!-- Load more popular categories -->
-{!! Html::script('assets/js/mpf.popular-categories.js') !!}
+{{ Html::script('assets/js/vendor/jquery-3.2.1.js') }}
+{{ Html::script('assets/js/vendor/bootstrap-3.1.1.js') }}
+{{ Html::script('assets/js/vendor/slick-carousel.js') }}
+<!-- Category-->
+{{ Html::script('assets/js/main/popular-categories.js') }}
+<!-- Navbar-->
+{{ Html::script('assets/js/main/navbar.js') }}
+{{ Html::script('assets/js/main/slidecategory.js') }}
+<!-- Slider-->
+{{ Html::script('assets/js/main/slider.js') }}
+<!-- Carousel-->
+{{ Html::script('assets/js/main/slick-carousel-custom.js') }}
 <script>
-$(document).on('ready', function() {
-    setTimeout(function() {
-        // Carousel
-        $('.regular').slick({
-            infinite: true,
-            slidesToShow: 5,
-            slidesToScroll: 1,
-            autoplay: true,
-            responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 5,
-                }
-            }, {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 3,
-                    arrows: false
-                }
-            }],
-        });
+$(function() {
+    // Navbar
+    $('.fj-navbar').affix({
+        offset: {
+            top: 504
+        }
+    });
+    $('.fj-navbar-mobile').affix({
+        offset: {
+            top: 215
+        }
     });
 });
 </script>
