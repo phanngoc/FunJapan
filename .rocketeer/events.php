bfc5ff555c7f086e7ee96cc667e32fee9eb93ded
TungPro <mr.nttung@gmail.com>
2017-04-19 09:30:35 +0700

    Add dev server config

--- .rocketeer/events.php
+++ .rocketeer/events.php
@@ -3,4 +3,5 @@ use Rocketeer\Facades\Rocketeer;
 Rocketeer::addTaskListeners('deploy', 'before-symlink', function ($task) {
     $task->runForCurrentRelease('npm run dev');
     $task->runForCurrentRelease('php artisan migrate --force');
+    $task->runForCurrentRelease('php artisan storage:link');
 });
