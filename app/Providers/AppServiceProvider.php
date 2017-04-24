0acf6f9191f52b67027f5eaa844c093bf5bd39c7
Viet Anh <nv2anh@gmail.com>
2017-04-24 14:35:24 +0700

    [PHP-252]: Don't check special characters of email address

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('email_update', function ($attribute, $value, $parameters, $validator) {
+            $regex = '/^[^!#$%^&*+-]+$/';
+
+            return preg_match($regex, $value);
+        });
     }
 
     /**
